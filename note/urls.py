ec95402f8d19299251786adfeb92c9eac92ff532
brajesh <brajesh@gmail.com>
2021-06-06 23:58:52 +0530

    Added login_view logout_view

--- note/urls.py
+++ note/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path("", views.home, name='home'),
+	path("login", views.login_view, name='login'),
+	path("logout", views.logout_view, name='logout'),
 	path('home/', views.index, name='index'),
 	path('addNote/', views.addNote, name='addNote'),
 	path('deleteNote/<int:note_id>/', views.deleteNote, name="deleteNote"),
--- note/views.py
+++ note/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render, reverse
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from .models import NewNote
 # Create your views here.
 
+# login
+def home(request):
+	if not request.user.is_authenticated:
+		return render(request, "note/login.html", {"message":None})
+	context = {
+		"user":request.user
+	}
+	return HttpResponseRedirect(reverse('index'))
+
+def login_view(request):
+	username = request.POST["username"]
+	password = request.POST["password"]
+	user = authenticate(request, username=username, password=password)
+	if user is not None:
+		login(request, user)
+		return HttpResponseRedirect(reverse('index'))
+	else:
+		return render(request, "note/login.html", {"message":"Invalid Credential"})
+
+def logout_view(request):
+	logout(request)
+	return render(request, "note/login.html", {"message":"Logged Out"})
+# login end
+
+# start notes
 def index(request):
     context = {
     "all_notes":NewNote.objects.all()
@@ -19,3 +45,5 @@ def deleteNote(request, note_id):
 	item_to_delete = NewNote.objects.get(id=note_id)
 	item_to_delete.delete()
 	return HttpResponseRedirect(reverse('index'))
+
+# end Notes
